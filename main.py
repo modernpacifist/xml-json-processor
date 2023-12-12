eee0a495ea7632ffb02c635e75481cf1cffde158
modernpacifist <modernpacifist@gmail.com>
2023-12-12 19:55:02 +0300

    proelium-exercitum:12/12/2023-19:55:02 automated backup commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import fastapi
+
+
+app = fastapi.FastAPI()
+
+
+@app.get("/")
+async def read_root():
+    return {"hello": "world"}
+
+
+@app.get("/api/json")
+async def process_json():
+    return {"hello": "world"}
+
+
+@app.get("/api/xml")
+async def process_xml():
+    return {"hello": "world"}
+
+
+def main():
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
